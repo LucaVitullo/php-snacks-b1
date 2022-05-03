ad82a1285f7f5829515afa5563231dcf9d59d4aa
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-05-03 16:57:27 +0200

    snacks 4 completato

--- snack-4/index.php
+++ snack-4/index.php
@@ -10,4 +10,10 @@ Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo par
 $paragrafo = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio possimus quis eveniet corporis ex obcaecati dolores aut. qui itaque assumenda harum enim corrupti minima id laborum quia nemo, inventore aspernatur.';
 
 
+$paragrafoNuovo = explode('.', $paragrafo);
+
+
+for ($i = 0; $i < count($paragrafoNuovo); $i++) {
+    echo '<p>' . $paragrafoNuovo[$i] . '</p>';
+}
 ?>
