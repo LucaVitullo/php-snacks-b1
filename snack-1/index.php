29447276a265bb0f92d6d9e563949890df9689cd
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-05-03 15:15:00 +0200

    snack 1 completato

--- snack-1/index.php
+++ snack-1/index.php
@@ -1,4 +1,46 @@
 <?php
-/* Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
-    Olimpia Milano - Cantù | 55-60
-    */
+/* Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
+Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
+Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60
+*/
+?>
+
+<?php
+$partite = [
+    [
+        'Casa' => 'Atlanta hawks',
+        'Ospiti' => 'Boston Celtics',
+        'PuntiCasa' => '67',
+        'PuntiOspiti' => '64'
+    ],
+    [
+        'Casa' => 'Chicago Bulls',
+        'Ospiti' => 'Orlando Magic',
+        'PuntiCasa' => '90',
+        'PuntiOspiti' => '76'
+    ],
+    [
+        'Casa' => 'Los Angeles Lakers ',
+        'Ospiti' => 'Utah Jazz',
+        'PuntiCasa' => '88',
+        'PuntiOspiti' => '79'
+    ],
+    [
+        'Casa' => 'San Antonio Spurs',
+        'Ospiti' => 'Golden State Warriors',
+        'PuntiCasa' => '81',
+        'PuntiOspiti' => '81'
+    ],
+];
+?>
+<p>
+
+    <?php
+    for ($i = 0; $i < count($partite); $i++) {
+        echo  $partite[$i]['Casa'] . ' - ' . $partite[$i]['Ospiti'] . ' ' . $partite[$i]['PuntiCasa'] . '|' . $partite[$i]['PuntiOspiti'] . ' <br>';
+    };
+    ?>
+
+
+</p>
